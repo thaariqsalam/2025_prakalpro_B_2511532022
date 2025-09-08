fa056310386a0954c3422e6a63d39d5aeb28fd0d
user <user@DESKTOP-FO4O9JP>
2025-09-08 11:46:26 +0700

    pratikum alpro pekan 1

---
+++ prakalpro_2025_B_2511532022/src/Pekan1/MembuatKue.java
@@ -0,0 +1,30 @@
+package Pekan1;
+
+public class MembuatKue {
+	public static void main(String[] args) {
+		makeBatter();
+		memanggang();
+		memanggang();
+		menghias();
+	}
+	//Langkah 1 : Buat adonan kue.
+	public static void makeBatter() {
+		System.out.println("Campur bahan kering");
+		System.out.println("Krim mentega dan gula");
+		System.out.println("Kocok telurnya");
+		System.out.println("Masukkan bahan kering");
+	}
+	//Langkah 2 : Panggang sekumpulan kue.
+	public static void memanggang() {
+		System.out.println("Setel suhu oven");
+		System.out.println("Setel pengatur suhu");
+		System.out.println("Masukkan kue ke dalam oven");
+		System.out.println("Biarkan cookie untuk di panggang");
+	}
+	//Langkah 3 : Hiasi Cookie
+	public static void menghias() {
+			System.out.println("Campur bahan untuk frosting");
+			System.out.println("Taburkan frosting dan taburan");
+	}
+}
+
